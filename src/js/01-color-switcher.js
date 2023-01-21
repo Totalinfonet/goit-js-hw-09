28711f8bd15234fef413c8135ebcd7978e005942
Totalinfonet <totalinfonet@gmail.com>
2023-01-21 19:37:27 +0200

    Color switcher done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,36 @@
-console.log('hello');
-alert('Hello world!');
+const refs = {
+  startBtn: document.getElementById('start-btn'),
+  stopBtn: document.getElementById('stop-btn'),
+  body: document.querySelector('body'),
+};
+
+let intervalId = null;
+
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  intervalId = setInterval(changeBodyColor, 1000);
+  toggleButtons();
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  toggleButtons();
+}
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+function toggleButtons() {
+  refs.startBtn.disabled = !refs.startBtn.disabled;
+  refs.stopBtn.disabled = !refs.stopBtn.disabled;
+}
