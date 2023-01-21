3f5bd25d0bf13df79e16ec2f7460dc6bbc1928ad
Totalinfonet <110982533+Totalinfonet@users.noreply.github.com>
2023-01-21 13:18:41 +0000

    Deploying to gh-pages from @ Totalinfonet/goit-js-hw-09@447ba774d840e96f5bb5b9b77f18d56b4769a4d2 🚀

---
+++ 01-color-switcher.2eceb994.js
@@ -0,0 +1,2 @@
+console.log("hello"),alert("Hello world!");
+//# sourceMappingURL=01-color-switcher.2eceb994.js.map
---
+++ 01-color-switcher.fb19f801.js
@@ -0,0 +1,2 @@
+console.log("hello"),alert("Hello world!");
+//# sourceMappingURL=01-color-switcher.fb19f801.js.map
